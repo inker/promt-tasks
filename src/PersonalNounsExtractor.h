e98e1432be991ad9cc3b001e5e80307aed082bbc
Anton Veselyev <anton.veselyev@gmail.com>
2014-06-22 18:31:42 +0400

    deleted unnecessary stuff

---
+++ src/PersonalNounsExtractor.h
@@ -0,0 +1,12 @@
+#include "stdafx.h"
+#include <string>
+#include <vector>
+#include <regex>
+
+class PersonalPronouns {
+	std::vector<std::string> personal_pronouns;
+
+public:
+	PersonalPronouns(const std::string& text) {
+	}
+};
